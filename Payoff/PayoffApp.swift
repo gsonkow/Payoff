96d5b64644fcb1eacae1dfcb98312a51d7819eb1
gsonkow <sonkowskyg@gmail.com>
2023-10-26 16:38:21 -0700

    Initial Commit

---
+++ Payoff/PayoffApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayoffApp.swift
+//  Payoff
+//
+//  Created by Gabriel Sonkowsky on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PayoffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
