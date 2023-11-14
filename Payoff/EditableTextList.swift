4ef4dec0914516eca186e1b936d87d1f6a7a7761
BryMad <75340105+BryMad@users.noreply.github.com>
2023-11-14 14:07:58 -0800

    add tags with right click feature added

--- Payoff/EditableTextList.swift
+++ Payoff/EditableTextList.swift
@@ -9,77 +9,122 @@ import Foundation
 import SwiftUI
 
 struct StoryBeat: Identifiable {
-    var id = UUID()
-    var text: String
-    var isScene: Bool
-    var tags: Set<String>
+  var id = UUID()
+  var text: String
+  var isScene: Bool
+  var tags: Set<String>
 }
 
-struct EditableTextList: View {
-    @State public var storyBeats: [StoryBeat] = [StoryBeat(text: "", isScene: true, tags: [])]
+struct TagsSidebar: View {
+    @Binding var tags: [String]
+    @State private var newTag: String = ""
+    @Binding var selectedTag: String?
 
     var body: some View {
-        List {
-            ForEach(storyBeats) { beat in
-                EditableTextRow(beat: $storyBeats[getIndex(for: beat)])
-                    .contextMenu {
-                        Button(action: {
-                            deleteBeat(beat)
-                        }) {
-                            Label("Delete", systemImage: "trash")
-                        }
+        VStack {
+            TextField("New Tag", text: $newTag, onCommit: {
+                if !newTag.isEmpty {
+                    tags.append(newTag)
+                    newTag = ""
+                }
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+
+            List(tags, id: \.self) { tag in
+                Text(tag)
+                    .onTapGesture {
+                        selectedTag = tag
                     }
             }
-            .onMove(perform: moveBeat)
         }
-        .listStyle(PlainListStyle())
-        .frame(minWidth: 200, idealWidth: 300, maxWidth: .infinity, minHeight: 200, idealHeight: 400, maxHeight: .infinity)
-        .toolbar {
+    }
+}
+
+
+
+struct EditableTextList: View {
+  @State public var storyBeats: [StoryBeat] = [StoryBeat(text: "", isScene: true, tags: [])]
+  @State public var tags: [String] = ["Love Plot", "Action"]
+  @State private var selectedTag: String?
+  
+  
+  var body: some View {
+    List {
+      ForEach(storyBeats) { beat in
+        EditableTextRow(beat: $storyBeats[getIndex(for: beat)])
+          .contextMenu {
             Button(action: {
-                addNewBeat()
+              deleteBeat(beat)
             }) {
-                Label("Add Beat", systemImage: "plus")
-            }.keyboardShortcut(.defaultAction)
-        }
+              Label("Delete", systemImage: "trash")
+            }
+            Menu("Add Tag") {
+              ForEach(tags, id: \.self) { tag in
+                Button(tag) {
+                  assignTag(tag, to: beat)
+                }
+              }
+            }
+          }
+      }
+      .onMove(perform: moveBeat)
     }
-
-    func moveBeat(from source: IndexSet, to destination: Int) {
-        storyBeats.move(fromOffsets: source, toOffset: destination)
+    .listStyle(PlainListStyle())
+    .frame(minWidth: 200, idealWidth: 300, maxWidth: .infinity, minHeight: 200, idealHeight: 400, maxHeight: .infinity)
+    .toolbar {
+      Button(action: {
+        addNewBeat()
+      }) {
+        Label("Add Beat", systemImage: "plus")
+      }.keyboardShortcut(.defaultAction)
     }
-
-    func addNewBeat() {
-        storyBeats.append(StoryBeat(text: "", isScene: true, tags: []))
+  }
+  
+  func assignTag(_ tag: String, to beat: StoryBeat) {
+    if var index = storyBeats.firstIndex(where: { $0.id == beat.id }) {
+      storyBeats[index].tags.insert(tag)
     }
-
-    func getIndex(for beat: StoryBeat) -> Int {
-        if let index = storyBeats.firstIndex(where: { $0.id == beat.id }) {
-            return index
-        }
-        return 0
+  }
+  
+  
+  func moveBeat(from source: IndexSet, to destination: Int) {
+    storyBeats.move(fromOffsets: source, toOffset: destination)
+  }
+  
+  
+  func addNewBeat() {
+    storyBeats.append(StoryBeat(text: "", isScene: true, tags: []))
+  }
+  
+  func getIndex(for beat: StoryBeat) -> Int {
+    if let index = storyBeats.firstIndex(where: { $0.id == beat.id }) {
+      return index
     }
-
-    func deleteBeat(_ beat: StoryBeat) {
-        if let index = storyBeats.firstIndex(where: { $0.id == beat.id }) {
-            storyBeats.remove(at: index)
-        }
+    return 0
+  }
+  
+  func deleteBeat(_ beat: StoryBeat) {
+    if let index = storyBeats.firstIndex(where: { $0.id == beat.id }) {
+      storyBeats.remove(at: index)
     }
+  }
 }
 
 
 
 struct EditableTextRow: View {
-    @Binding var beat: StoryBeat
-    enum FocusField: Hashable {
-        case field
-    }
-    
-    @FocusState private var focusedField: FocusField?
-    
-    var body: some View {
-        TextField("Enter text", text: $beat.text)
-            .focused($focusedField, equals: .field)
-            .onAppear {self.focusedField = .field}
-    }
+  @Binding var beat: StoryBeat
+  enum FocusField: Hashable {
+    case field
+  }
+  @FocusState private var focusedField: FocusField?
+  
+  var body: some View {
+    TextField("Enter text", text: $beat.text)
+      .focused($focusedField, equals: .field)
+      .onAppear {self.focusedField = .field}
+  }
 }
 
 #Preview {
